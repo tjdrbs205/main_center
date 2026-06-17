abd104cb7ec4006f9b14e5d9a77d5a098754592f
tjdrbs205 <tjdrbs20568@gmail.com>
2026-06-17 09:31:03 +0900

    fix: GCI Index형식에 필요한 타입이 header에 없어 생기는 문제를 해결했습니다.

--- src/setting/setting.service.ts
+++ src/setting/setting.service.ts
@@ -56,7 +56,7 @@ export class SettingService {
         method: 'HEAD',
         headers: {
           'Authorization': `Bearer ${bearer}`,
-          'Accept': 'application/vnd.docker.distribution.manifest.v2+json, application/vnd.oci.image.manifest.v1+json'
+          'Accept': 'application/vnd.oci.image.index.v1+json, application/vnd.docker.distribution.manifest.list.v2+json, application/vnd.docker.distribution.manifest.v2+json, application/vnd.oci.image.manifest.v1+json'
         }
       });
 
